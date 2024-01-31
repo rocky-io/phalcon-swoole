946dbb73585d764a489548e4862c3e5ce5630adf
rocky <rockyskate@163.com>
2024-01-31 11:00:13 +0800

    增加CORS

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -36,6 +36,24 @@ class BaseController extends Controller
         $this->swooleRequest = Coroutine::getContext()['request'];
         /* 重新定义response */
         $this->response = (new Response())->setStatusCode(200, 'OK');
+        //CORS
+        $this->response
+            ->setHeader(
+                'Access-Control-Allow-Origin',
+                '*'
+            )
+            ->setHeader(
+                'Access-Control-Allow-Methods',
+                'GET,PUT,POST,DELETE,OPTIONS'
+            )
+            ->setHeader(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With, Content-Range, Content-Disposition, Content-Type, Authorization'
+            )
+            ->setHeader(
+                'Access-Control-Allow-Credentials',
+                'true'
+            );
         return true;
     }
 
